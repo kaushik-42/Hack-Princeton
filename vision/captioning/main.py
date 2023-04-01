8e57f96b840c0b451c4f2eef54cd65f8454bca95
kaushik-42 <kaushik052001@yahoo.com>
2023-04-01 02:12:30 -0400

    captioning update

--- vision/captioning/main.py
+++ vision/captioning/main.py
@@ -2,36 +2,51 @@
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
 from PIL import Image
+from object_detection import ObjectDetector
 
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+model = VisionEncoderDecoderModel.from_pretrained(
+    "nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained(
+    "nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained(
+    "nlpconnect/vit-gpt2-image-captioning")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 
-
 max_length = 16
 num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+
 def predict_step(image_paths):
-  images = []
-  for image_path in image_paths:
-    i_image = Image.open(image_path)
-    if i_image.mode != "RGB":
-      i_image = i_image.convert(mode="RGB")
+    images = []
+    for image_path in image_paths:
+        i_image = Image.open(image_path)
+        if i_image.mode != "RGB":
+            i_image = i_image.convert(mode="RGB")
 
-    images.append(i_image)
+        images.append(i_image)
 
-  pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-  pixel_values = pixel_values.to(device)
+    pixel_values = feature_extractor(
+        images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
 
-  output_ids = model.generate(pixel_values, **gen_kwargs)
+    output_ids = model.generate(pixel_values, **gen_kwargs)
 
-  preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-  preds = [pred.strip() for pred in preds]
-  return preds
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    return preds
 
 
-print(predict_step(['p.jpeg']))# ['a woman in a hospital bed with a woman in a hospital bed']
+if __name__ == "__main__":
+    object_detector = ObjectDetector()
+    object_detector.save_bounding_boxes(
+        '/Users/kaushiktummalapalli/Desktop/Image Processing/Hack-Princeton/vision/captioning/sai.jpeg')
+
+    predict_step(
+        "/Users/kaushiktummalapalli/Desktop/Image Processing/Hack-Princeton/vision/captioning/sai.jpeg")
+    #
+
+# print(predict_step(['p.jpeg']))# ['a woman in a hospital bed with a woman in a hospital bed']
