8518b3bc9ee56d746d10fb6e7425b172b6edcd27
kaushik-42 <kaushik052001@yahoo.com>
2023-04-01 01:50:04 -0400

    updated model_hf.py

--- vision/object detection/model_hf.py
+++ vision/object detection/model_hf.py
@@ -1,3 +1,42 @@
+
+import cv2
+from PIL import Image
+from transformers import pipeline
+
+
+class ObjectDetector:
+    def __init__(self, model_name='object-detection', image_classes=['traffic light', 'traffic sign', 'person']):
+        self.image_classes = image_classes
+        self.object_detector = pipeline(model_name)
+
+    def detect_objects(self, image_path):
+        image = Image.open(image_path)
+        results = self.object_detector(image)
+        filtered_results = [
+            obj for obj in results if obj['label'] in self.image_classes]
+        return filtered_results
+
+    def save_bounding_boxes(self, image_path):
+        results = self.detect_objects(image_path)
+        img = cv2.imread(image_path)
+        for i, result in enumerate(results):
+            xmin = result['box']['xmin']
+            ymin = result['box']['ymin']
+            xmax = result['box']['xmax']
+            ymax = result['box']['ymax']
+            label = result['label']
+
+            cropped_img = img[ymin:ymax, xmin:xmax]
+            file_name = label.replace(" ", "_") + "_" + str(i+1) + ".jpg"
+            cv2.imwrite(file_name, cropped_img)
+
+
+object_detector = ObjectDetector()
+object_detector.save_bounding_boxes(
+    '/Users/kaushiktummalapalli/Desktop/Image Processing/Hack-Princeton/vision/captioning/sai.jpeg')
+
+
+"""
 import cv2
 import requests
 from PIL import Image
@@ -5,14 +44,40 @@ from transformers import pipeline
 # from io import BytesIO
 
 # Download an image with cute cats
-url = "/Users/kaushiktummalapalli/Desktop/Image Processing/Hack-Princeton/vision/captioning/p.jpeg"
+url = "/Users/kaushiktummalapalli/Desktop/Image Processing/Hack-Princeton/vision/captioning/sai.jpeg"
 # image_data = requests.get(url, stream=True).raw
 image = Image.open(url)
 
 # Allocate a pipeline for object detection
 object_detector = pipeline('object-detection')
+
 # print(object_detector(image))
-helper = object_detector(image)
+result = object_detector(image)
+image_classes = ['traffic light', 'traffic sign', 'person']
+filtered_results = [obj for obj in result if obj['label'] in image_classes]
+
+# Loop through each filtered result and create an image for each bounding box
+for i, result in enumerate(filtered_results):
+    xmin = result['box']['xmin']
+    ymin = result['box']['ymin']
+    xmax = result['box']['xmax']
+    ymax = result['box']['ymax']
+    label = result['label']
+
+    # Crop the original image to get the bounding box image
+    # bbox_image = image.crop((xmin, ymin, xmax, ymax))
+
+    # Save the bounding box image to a file
+    # bbox_image.save(f"bounding_box_{i}.jpg")
+    img = cv2.imread(url)
+    cropped_img = img[ymin:ymax, xmin:xmax]
+    file_name = label.replace(" ", "_") + "_" + str(i+1) + ".jpg"
+    cv2.imwrite(file_name, cropped_img)
+
+
+
+
+# OLD 
 
 
 # Load the image
@@ -20,7 +85,7 @@ img = cv2.imread(
     "/Users/kaushiktummalapalli/Desktop/Image Processing/Hack-Princeton/vision/captioning/p.jpeg")
 
 # Loop through each object and draw the corresponding bounding box
-for obj in helper:
+for obj in filtered_helper:
     xmin = obj['box']['xmin']
     ymin = obj['box']['ymin']
     xmax = obj['box']['xmax']
@@ -41,7 +106,8 @@ cv2.imwrite('test.jpg', img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-"""
+
+# Results and bounding boxes for the labels we generated:
 [{'score': 0.9982201457023621,
   'label': 'remote',
   'box': {'xmin': 40, 'ymin': 70, 'xmax': 175, 'ymax': 117}},
